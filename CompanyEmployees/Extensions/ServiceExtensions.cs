45d7338bf31e60ccdb449cfaf97bb4c75b07a12e
Robinson Souza <Robinsond76@gmail.com>
2021-02-18 17:13:54 -0500

    Created Entities, RepositoryContext and added to Startup.

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,16 +1,16 @@
 ﻿using Contracts;
+using Entities;
 using LoggerService;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CompanyEmployees.Extensions
 {
     public static class ServiceExtensions
     {
+        //Cors
         public static void ConfigureCors(this IServiceCollection services) =>
             services.AddCors(options =>
             {
@@ -20,15 +20,22 @@ namespace CompanyEmployees.Extensions
                 .AllowAnyHeader());
             });
 
+        //IIS configuration
         public static void ConfigureIISIntegration(this IServiceCollection services) =>
             services.Configure<IISOptions>(options =>
             {
 
             });
 
+        //logger
         public static void ConfigureLoggerService(this IServiceCollection services) =>
             services.AddScoped<ILoggerManager, LoggerManager>();
 
+        //Sql Server and Connection String
+        public static void ConfigureSqlContext(this IServiceCollection services,
+            IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(opts =>
+            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 
     }
 }
