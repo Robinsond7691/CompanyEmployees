f3a36d6f18f98af9300aa487d080c8ab8907993e
Robinson Souza <Robinsond76@gmail.com>
2021-02-19 12:51:01 -0500

    Added Employees Controller, Dto, and an action

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -3,6 +3,7 @@ using Entities;
 using Entities.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Repository
@@ -14,5 +15,9 @@ namespace Repository
         {
 
         }
+
+        public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) =>
+            FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+            .OrderBy(e => e.Name);
     }
 }
