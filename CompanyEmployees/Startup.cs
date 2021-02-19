f832e093d927365ce5bc2b25ba1c9986a65ffd18
Robinson Souza <Robinsond76@gmail.com>
2021-02-19 11:17:30 -0500

    Added Automapper

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -38,6 +38,9 @@ namespace CompanyEmployees
             services.ConfigureSqlContext(Configuration);
             services.ConfigureRepositoryManager();
 
+            services.AddAutoMapper(typeof(Startup));
+
+
             services.AddControllers();
         }
 
