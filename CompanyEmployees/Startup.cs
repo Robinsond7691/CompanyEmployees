fbf4b75381749b87f20e1709bbd6cc7f21221513
Robinson Souza <Robinsond76@gmail.com>
2021-02-19 13:53:52 -0500

    Added XML content negotiation

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -42,7 +42,10 @@ namespace CompanyEmployees
             services.AddAutoMapper(typeof(Startup));
 
 
-            services.AddControllers();
+            services.AddControllers(config =>
+            {
+                config.RespectBrowserAcceptHeader = true;
+            }).AddXmlDataContractSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
