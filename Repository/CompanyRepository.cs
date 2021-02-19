4cd2628a655aeced32234597f12c0bf441742b19
Robinson Souza <Robinsond76@gmail.com>
2021-02-19 11:01:22 -0500

    Added GetAllCompanies Action with CompanyDto. Mapped manually

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -3,6 +3,7 @@ using Entities;
 using Entities.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Repository
@@ -12,7 +13,11 @@ namespace Repository
         public CompanyRepository(RepositoryContext repositoryContext) 
             : base(repositoryContext)
         {
-
         }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+            FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
     }
 }
