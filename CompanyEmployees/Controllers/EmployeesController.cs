fa53a5399ebd7a1c3505ed571668005961d683de
Robinson Souza <Robinsond76@gmail.com>
2021-02-23 13:44:02 -0500

    Improved paging feature, added metadata to response header

--- CompanyEmployees/Controllers/EmployeesController.cs
+++ CompanyEmployees/Controllers/EmployeesController.cs
@@ -7,6 +7,7 @@ using Entities.RequestFeatures;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -42,6 +43,9 @@ namespace CompanyEmployees.Controllers
             }
 
             var employeesFromDb = await _repository.Employee.GetEmployeesAsync(companyId, employeeParameters, trackChanges: false);
+
+            //Adds metadata from PagedList to the Response header for pagination
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(employeesFromDb.MetaData));
 
             var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
             return Ok(employeesDto);
--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -17,14 +17,20 @@ namespace Repository
             :base(repositoryContext)
         {
         }
-        public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, 
+
+        public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, 
             EmployeeParameters employeeParameters, bool trackChanges)
         {
-           return await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
-            .OrderBy(e => e.Name)
-            .Skip((employeeParameters.PageNumber -1) * employeeParameters.PageSize)
-            .Take(employeeParameters.PageSize)
-            .ToListAsync();
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId),
+                trackChanges)
+                    .OrderBy(e => e.Name)
+                    .ToListAsync();
+
+            return PagedList<Employee>.ToPagedList(
+                employees,
+                employeeParameters.PageNumber,
+                employeeParameters.PageSize);
+
         }
 
         public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
