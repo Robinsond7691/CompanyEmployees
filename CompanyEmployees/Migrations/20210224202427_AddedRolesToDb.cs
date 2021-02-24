349ad2f4ea20252dcc6496e7aa7604a1de89e1d9
Robinson Souza <Robinsond76@gmail.com>
2021-02-24 15:26:30 -0500

    Configured manager and admin roles, added new migration

---
+++ CompanyEmployees/Migrations/20210224202427_AddedRolesToDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CompanyEmployees.Migrations
+{
+    public partial class AddedRolesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c21a1686-ffa4-4f77-93d0-d8c00094121d", "691a89b8-1fca-46be-b3e1-34da88bb16a0", "Manager", "MANAGER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "69f77606-5930-4d0f-b9af-abb3acc8f141", "ba595c2f-c3ce-4712-acc8-48918164586d", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "69f77606-5930-4d0f-b9af-abb3acc8f141");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c21a1686-ffa4-4f77-93d0-d8c00094121d");
+        }
+    }
+}
